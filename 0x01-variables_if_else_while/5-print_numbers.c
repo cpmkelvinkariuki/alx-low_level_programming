78a058f211abd25f0405fead4e4f79fc615da3ad
cpmkelvinkariuki <cmkelvinkariuki@gmail.com>
2022-12-09 18:41:54 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entrt point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	putchar('n');
+	return (0);
+}
