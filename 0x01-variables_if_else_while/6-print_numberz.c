bfb4f0dede5e5413aa613ea27946187ffc9d180e
cpmkelvinkariuki <cmkelvinkariuki@gmail.com>
2022-12-09 18:48:37 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * maain - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+	
+	x=0;
+	while
+		(x < 10 ) {
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return(0);
+}
