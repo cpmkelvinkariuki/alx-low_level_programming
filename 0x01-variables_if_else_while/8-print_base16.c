6c780b0859afd5726dc0dc16502caed0c0ab14d1
cpmkelvinkariuki <cmkelvinkariuki@gmail.com>
2022-12-09 19:05:28 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return = 0 (success)
+ */
+
+int main (void)
+{
+	char x;
+
+	int y;
+
+	x = 'a';
+	y = 0;
+	while
+		(y < 10) {
+			putchar(y + '0');
+			y++;
+		}
+	while
+		(x <= 'f') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
